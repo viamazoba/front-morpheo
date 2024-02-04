8955e4576edbd5188f285bcbf952ebb6155d038b
viamazoba <viamazoba@unal.edu.co>
2024-02-03 20:33:59 -0500

    feat: Se finaliza banner de homepage.

---
+++ src/components/02-molecules/BannerHome.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BannerHome as Banner } from "./BannerHome.tsx"
+// import { MenuCard as CardMenu } from "./MenuCard.tsx"
+
+export default {
+    title: 'Molecules/BannerHome',
+    // tags: ['autodocs'],
+};
+
+export const BannerHome = (args) => <Banner {...args} />;
