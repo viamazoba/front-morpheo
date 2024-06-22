0103462e619004f7908c938647015510f8c37521
viamazoba <viamazoba@unal.edu.co>
2024-06-22 00:39:38 -0500

    feat: Se crea icono custom, además se modifica el acordeon.

--- src/components/01-atoms/AccordionTest.tsx
+++ src/components/01-atoms/AccordionTest.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react'
 import Image from 'next/image'
 import arrow from '@icons/angulo-abajo.svg'
+import { Remove } from './custom-icons/Remove'
 
 type AccordionTestInterface = {
     children: React.ReactNode;
@@ -9,6 +10,9 @@ type AccordionTestInterface = {
 export const AccordionTest: FC<AccordionTestInterface> = ({
     children = <p>Contenido</p>
 }) =>{
+    // TODO: Sobre saltar este elemento
+    // Esto para que entienda que pregunta dejó incompleta
+    
     const [openTab, setOpenTab] = useState<boolean>(false)
     const handleOpenTab = ()=>{
         setOpenTab(!openTab)
@@ -17,13 +21,17 @@ export const AccordionTest: FC<AccordionTestInterface> = ({
         <div className=' bg-principal-150 rounded-12 overflow-hidden'>
             <div className='flex items-center justify-between min-h-14 shadow-md px-3 py-2'>
                 <p>Pregunta #1</p>
-                <div className='cursor-pointer px-1 py-1' onClick={handleOpenTab}>
+                <div className='flex px-1 py-1'>
+                    <Remove 
+                        classSvg='w-7 h-7 hover:bg-principal-500 rounded opacity-90 cursor-pointer mr-3'
+                    />
                     <Image
                         src={arrow}
                         width={50}
                         height={50}
                         alt='flecha acordeon'
-                        className={`w-8 h-8 ${openTab &&'rotate-180'}`}
+                        className={`w-7 h-7 ${openTab &&'rotate-180'} rounded cursor-pointer hover:bg-principal-550 hover:bg-opacity-80`}
+                        onClick={handleOpenTab}
                     />
                 </div>
             </div>
