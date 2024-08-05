5a52677c87d5c54b4011591490b8ab075a07b3d8
Victor Mazo <viamazoba@unal.edu.co>
2024-08-04 22:34:48 -0500

    feat: Se crea primera versión del formulario para la creación de exámenes.

---
+++ src/components/02-molecules/FormSubject.stories.js
@@ -0,0 +1,27 @@
+import { FormSubject as Form } from './FormSubject';
+
+const meta = {
+    title: 'Molecules/FormSubject',
+
+    component: Form,
+    argTypes: {
+        // nameImage: {
+        //     control: {
+        //         type: 'select',
+        //         options: ['atom', 'exam', 'molecule', 'microscope']
+        //     }
+        // },
+        //labelText: { control: 'text' },
+    }
+};
+
+
+
+export default meta;
+
+export const FormSubject = {
+    args: {
+        // nameImage: 'atom',
+        //labelText: 'Ingrese texto',
+    }
+};
