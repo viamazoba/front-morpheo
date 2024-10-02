5f4727f8769c15bbc0b5defdd8e5e09e53a09ae5
Victor Mazo <viamazoba@unal.edu.co>
2024-10-01 21:36:31 -0500

    feat: Se inatala la librería de heroIcons y se finaliza filtro para exámenes.

--- src/components/02-molecules/filters/FilterCardExam.tsx
+++ src/components/02-molecules/filters/FilterCardExam.tsx
@@ -1,6 +1,8 @@
 import { CustomInput } from "@/components/01-atoms/CustomInput"
 import { CustomSelect } from "@/components/01-atoms/CustomSelect"
 import { ChangeEvent, useState } from "react"
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline'
+import { GroupButtons } from "../GroupButtons"
 
 const optionsExample = [
     {
@@ -21,16 +23,54 @@ const optionsExample = [
     },
 ]
 
-export const FilterCardExam = () => {
+const optionsGrades = [
+    {
+        idOption: '9',
+        value: '9'
+    },
+    {
+        idOption: '10',
+        value: '10'
+    },
+    {
+        idOption: '11',
+        value: '11'
+    }
+]
+
+const arrayButtons = [{
+    nameButton: 'Aplicar',
+    typeButton: 'blueTwoButton',
+    classButton: '',
+},
+{
+    nameButton: 'Limpiar',
+    typeButton: 'redButton',
+    classButton: '',
+},
+]
+
+type FilterCardExamProps = {
+    classNameContainer: string
+}
+
+export const FilterCardExam = ({
+    classNameContainer
+}: FilterCardExamProps) => {
 
     const [filterValues, setFilterValues] = useState({
         typeExam: '',
         grade: '',
-        year: ''
+        year: '',
+        creationYear: '',
+        creationMonth: '',
+        creationDay: ''
     })
 
+    const [advancedFilter, setAdvancedFilter] = useState(false)
+
     const handleValues = (e: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
-        console.log('Valor enviado: ', e.target.value)
+        /* console.log('Valor enviado: ', e.target.value) */
         setFilterValues({
             ...filterValues,
             [e.target.id]: e.target.value
@@ -38,36 +78,97 @@ export const FilterCardExam = () => {
     }
 
     return (
-        <section>
-            <div>
-                <CustomSelect idSelect={filterValues.typeExam} label={"Tipo de examen"} options={optionsExample} handleSeletedOption={handleValues} />
+        <section className={`flex flex-col gap-y-5 ${classNameContainer}`}>
+            <div className="bg-principal-100 rounded-8 py-1 px-1 w-full">
+                <h2 className="text-principal-150 text-22 text-center">Filtro Exámenes</h2>
             </div>
             <div>
-                <CustomInput
-                    idInput={"grade"}
-                    labelInput={"Grado: "}
-                    typeInput={"number"}
-                    value={filterValues.grade}
-                    regex={/^[0-9]+$/}
-                    onChange={handleValues}
+                <CustomSelect
+                    idSelect={filterValues.typeExam}
+                    label={"Tipo de examen"}
+                    options={optionsExample}
+                    handleSeletedOption={handleValues}
+                />
+            </div>
+            <div>
+                <CustomSelect
+                    idSelect={filterValues.year}
+                    label={"Grado:"}
+                    options={optionsGrades}
+                    handleSeletedOption={handleValues}
                 />
             </div>
             <div>
                 <CustomInput
                     idInput={"year"}
-                    labelInput={"Año: "}
+                    labelInput={"Año Examen: "}
                     typeInput={"number"}
                     value={filterValues.year}
                     regex={/^[0-9]+$/}
                     onChange={handleValues}
+                    placeholderInput="2011"
                 />
             </div>
-            <div>
-                Fecha
+
+            {
+
+                <div className={`flex flex-col transition-all duration-200 ease-in-out transform ${advancedFilter ? 'gap-y-2 opacity-100 scale-100' : 'gap-y-0 opacity-0 scale-95 h-0'}`}>
+                    <hr className={`text-principal-50 my-2 ${advancedFilter && 'my-0'}`} />
+                    <p className="font-semibold mb-1">Fecha de creación</p>
+                    <CustomInput
+                        idInput={"creationYear"}
+                        labelInput={"Año: "}
+                        typeInput={"number"}
+                        value={filterValues.creationYear}
+                        regex={/^[0-9]+$/}
+                        onChange={handleValues}
+                        placeholderInput="2022"
+                        classContainer={`${!advancedFilter && 'hidden'}`}
+                    />
+                    <CustomInput
+                        idInput={"creationMonth"}
+                        labelInput={"Mes: "}
+                        typeInput={"number"}
+                        value={filterValues.creationMonth}
+                        regex={/^[0-9]+$/}
+                        onChange={handleValues}
+                        placeholderInput="9"
+                        classContainer={`${!advancedFilter && 'hidden'}`}
+                    />
+                    <CustomInput
+                        idInput={"creationDay"}
+                        labelInput={"Día: "}
+                        typeInput={"number"}
+                        value={filterValues.creationDay}
+                        regex={/^[0-9]+$/}
+                        onChange={handleValues}
+                        placeholderInput="24"
+                        classContainer={`${!advancedFilter && 'hidden'}`}
+                    />
+                </div>
+            }
+            <div
+                className="flex flex-col items-center cursor-pointer hover:text-principal-100 hover:font-medium"
+                onClick={() => setAdvancedFilter(!advancedFilter)}
+            >
+                {
+                    advancedFilter ?
+                        <p>Mostrar Menos</p>
+                        :
+                        <p>Filtro avanzado</p>
+                }
+                <ChevronDoubleDownIcon
+                    className={`my-1 max-w-6 ${advancedFilter && 'rotate-180'}`}
+                />
             </div>
+
             <div>
-                Fecha creación
+                <GroupButtons
+                    arrayButtons={arrayButtons}
+                    classContainer="flex flex-col gap-y-1.5 mt-2"
+                />
             </div>
+
         </section>
     )
 }
