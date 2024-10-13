9aaaa3997e17c9e2dc2d009c4ea22880f0a98489
Victor Mazo <viamazoba@unal.edu.co>
2024-10-12 21:40:51 -0500

    feat: Se crea vista para ver resumen de los grupos y los componentes necesarios para esta vista, como filtro y card. Además se soluciona bug en vista de registro de estudiantes.

---
+++ src/components/02-molecules/cards/CardGroupDetail.tsx
@@ -0,0 +1,102 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline"
+import { useState } from "react"
+
+export const CardGroupDetail = () => {
+
+    const [isTestVisible, setIsTestVisible] = useState(false)
+    const [isAssigmentsVisible, setIsAssigmentsVisible] = useState(false)
+
+
+    const handleVisibleMenus = (menu: string) => {
+        if (menu === '1') {
+            setIsTestVisible(!isTestVisible)
+        } else if (menu === '2') {
+            setIsAssigmentsVisible(!isAssigmentsVisible)
+        }
+    }
+
+    return (
+        <div className="bg-principal-150 rounded-8 shadow-xl px-4 py-3 max-w-73 max-h-fit">
+            <h2 className="text-principal-350">
+                Grupo&nbsp;
+                <span>11-2</span>
+            </h2>
+            <hr className="border-principal-320 mt-1 mb-3" />
+            <div>
+                <div className="grid grid-cols-2 gap-3">
+                    <p className="font-semibold text-principal-180">
+                        Estudiantes:&nbsp;
+                        <span className="font-normal text-principal-350">23</span>
+                    </p>
+                    <p className="font-semibold text-principal-180">
+                        Año:&nbsp;
+                        <span className="font-normal text-principal-350">2024</span>
+                    </p>
+                </div>
+                <div className="my-2 relative">
+                    <p className="flex font-semibold text-principal-180 mb-1">
+                        Test Realizados&nbsp;(0)
+                        <ChevronDownIcon
+                            className={`text-principal-350  cursor-pointer rounded ml-1 p-0.5 w-6 h-auto hover:bg-principal-100 hover:text-principal-150 ${isTestVisible && 'rotate-180'}`}
+                            onClick={() => handleVisibleMenus('1')}
+                        />
+                    </p>
+                    <div className={`bg-principal-250 rounded-2 transition-all ease-in-out ${isTestVisible ? 'opacity-100 py-2 px-1 mt-2' : 'opacity-0 h-0 absolute -z-1'}`}>
+                        <table>
+                            <thead className="border-b-2 border-b-principal-150">
+                                <tr>
+                                    <th className="text-left px-1 min-w-32">Examen</th>
+                                    <th className="text-left px-1 min-w-32">Fecha</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="border-b-2 border-principal-150 last:border-principal-250">
+                                    <td className="px-1 pt-1">
+                                        ICFES
+                                    </td>
+                                    <td className="px-1 pt-1">
+                                        25/08/2024
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="my-2 relative">
+                    <p className="flex font-semibold text-principal-180">
+                        Test Asignados&nbsp;(1)
+                        <ChevronDownIcon
+                            className={`text-principal-350  cursor-pointer rounded ml-1 p-0.5 w-6 h-auto hover:bg-principal-100 hover:text-principal-150 ${isAssigmentsVisible && 'rotate-180'}`}
+                            onClick={() => handleVisibleMenus('2')}
+                        />
+                    </p>
+                    <div className={`bg-principal-250 rounded-2 transition-all ease-in-out ${isAssigmentsVisible ? 'opacity-100 py-2 px-1 mt-2' : 'opacity-0 h-0 absolute -z-1'}`}>
+                        <table>
+                            <thead className="border-b-2 border-b-principal-150">
+                                <tr>
+                                    <th className="text-left px-1 min-w-32">Examen</th>
+                                    <th className="text-left px-1 min-w-32">Fecha</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="border-b-2 border-principal-150 last:border-principal-250">
+                                    <td className="px-1 pt-1">
+                                        ICFES
+                                    </td>
+                                    <td className="px-1 pt-1">
+                                        25/08/2024
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <hr className="border-principal-320 mb-2 mt-3" />
+                <div className="flex flex-col">
+                    <p className="font-semibold text-principal-180 text-15 italic">Fecha de creación</p>
+                    <p className="font-normal text-principal-350 text-15 italic">27/10/2024</p>
+                </div>
+            </div>
+        </div>
+    )
+}
