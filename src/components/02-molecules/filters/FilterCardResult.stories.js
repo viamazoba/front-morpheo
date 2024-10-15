d78c9a6163f56341408760ce7b6c3504dbb991a5
Victor Mazo <viamazoba@unal.edu.co>
2024-10-14 22:08:57 -0500

    feat: Se crea filtro para resultados y la vista para los resultados de los exámenes.

---
+++ src/components/02-molecules/filters/FilterCardResult.stories.js
@@ -0,0 +1,27 @@
+import { FilterCardResult as Filter } from './FilterCardResult.tsx';
+
+const meta = {
+    title: 'Molecules/Filters/FilterCardResult',
+
+    component: Filter,
+    argTypes: {
+        // nameImage: {
+        //     control: {
+        //         type: 'select',
+        //         options: ['atom', 'exam', 'molecule', 'microscope']
+        //     }
+        // },
+        // textCard: { control: 'text' },
+    }
+};
+
+
+
+export default meta;
+
+export const FilterCardResult = {
+    args: {
+        // nameImage: 'atom',
+        // textCard: 'Explora',
+    }
+};
