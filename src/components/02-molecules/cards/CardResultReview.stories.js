b2b15c13728248100b5f2c5fe289d9bace26bdd6
Victor Mazo <viamazoba@unal.edu.co>
2024-10-14 21:16:46 -0500

    feat: Se crea card resumen para los resultados del examen.

---
+++ src/components/02-molecules/cards/CardResultReview.stories.js
@@ -0,0 +1,27 @@
+import { CardResultReview as Card } from './CardResultReview';
+
+const meta = {
+    title: 'Molecules/Cards/CardResultReview',
+
+    component: Card,
+    argTypes: {
+        // nameImage: {
+        //     control: {
+        //         type: 'select',
+        //         options: ['atom', 'exam', 'molecule', 'microscope']
+        //     }
+        // },
+        // textCard: { control: 'text' },
+    }
+};
+
+
+
+export default meta;
+
+export const CardResultReview = {
+    args: {
+        // nameImage: 'atom',
+        // textCard: 'Explora',
+    }
+};
