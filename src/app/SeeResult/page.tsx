f9d883545a11153c10e713f2fdff189915d42e59
Victor Mazo <viamazoba@unal.edu.co>
2024-10-15 22:16:04 -0500

    feat: Se genera segunda región para la vista de resultados.

--- src/app/SeeResult/page.tsx
+++ src/app/SeeResult/page.tsx
@@ -25,7 +25,7 @@ export default function SeeResult() {
 
     return (
         <section className="my-8 min-h-lvh">
-            <section className=" max-w-screen-sm">
+            <section className="max-w-screen-sm">
                 <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2">
                     <div className="flex text-20">
                         <p className="font-semibold text-principal-150 py-1">Grado:&nbsp;</p>
@@ -38,7 +38,7 @@ export default function SeeResult() {
                 </div>
                 <AccordionTest
                     title="Resultados por Materia"
-                    classNameTitle="font-semibold"
+                    classNameTitle="font-semibold text-principal-350"
                     isRemove={false}
                 >
                     <div className="px-1 pt-2 pb-5">
@@ -48,7 +48,7 @@ export default function SeeResult() {
                         </div>
                         {
                             exampleResults.map((result, index) => (
-                                <div key={index} className="grid grid-cols-2 border-b border-b-principal-50 py-1.5 w-full">
+                                <div key={index} className="grid grid-cols-2 border-b border-b-principal-50 py-1.5 w-full even:bg-principal-200">
                                     <p>{result.subject}</p>
                                     <p>{result.result}</p>
                                 </div>
@@ -56,6 +56,68 @@ export default function SeeResult() {
                         }
                     </div>
                 </AccordionTest>
+            </section>
+
+            <section className="max-w-screen-sm mt-8">
+                <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2">
+                    <p className="font-semibold text-20 text-principal-150 py-1">Temas a Mejorar</p>
+                </div>
+                <p className="bg-principal-200 rounded py-2 px-2">
+                    Las preguntas que se muestran a continuación son las que cumplen con la regla de Paretto, o en otras palabras, el 20% de las preguntas entre los estudiantes que causan el 80% del fallo en el examen.
+                </p>
+                <div className="px-1 pt-2 pb-1">
+                    <AccordionTest
+                        title="Preguntas con Mayor cantidad de fallos"
+                        classNameTitle="font-semibold text-principal-350"
+                        isRemove={false}
+                    >
+                        <div className="px-1 pt-2 pb-5">
+                            <div className="grid grid-cols-2 border-b border-b-principal-50 py-1 w-full">
+                                <p className="font-semibold">Materia</p>
+                                <p className="font-semibold">Descargar</p>
+                            </div>
+                            {
+                                exampleResults.map((result, index) => (
+                                    <div key={index} className="grid grid-cols-2 border-b border-b-principal-50 py-1.5 w-full even:bg-principal-200">
+                                        <p>{result.subject}</p>
+                                        <p
+                                            className="text-principal-100 border-b w-fit"
+                                        >
+                                            Generar PDF
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </AccordionTest>
+                </div>
+
+                <div className="px-1 pt-2 pb-5">
+                    <AccordionTest
+                        title="Preguntas con Mayor Tiempo de Respuesta"
+                        classNameTitle="font-semibold text-principal-350"
+                        isRemove={false}
+                    >
+                        <div className="px-1 pt-2 pb-5">
+                            <div className="grid grid-cols-2 border-b border-b-principal-50 py-1 w-full">
+                                <p className="font-semibold">Materia</p>
+                                <p className="font-semibold">Descargar</p>
+                            </div>
+                            {
+                                exampleResults.map((result, index) => (
+                                    <div key={index} className="grid grid-cols-2 border-b border-b-principal-50 py-1.5 w-full even:bg-principal-200">
+                                        <p>{result.subject}</p>
+                                        <p
+                                            className="text-principal-100 border-b w-fit"
+                                        >
+                                            Generar PDF
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </AccordionTest>
+                </div>
             </section>
         </section>
     )
