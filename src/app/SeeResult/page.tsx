fdc33189ba72846c6aa6f8f27029898e48979d9e
Victor Mazo <viamazoba@unal.edu.co>
2024-10-16 21:13:49 -0500

    feat:Se finaliza vista de resultados para un examen particular.

--- src/app/SeeResult/page.tsx
+++ src/app/SeeResult/page.tsx
@@ -21,10 +21,31 @@ const exampleResults = [
     },
 ]
 
+const exampleStudents = [
+    {
+        id: '1',
+        name: 'Juan Carlos Arboleda Castro',
+        result: '78,94%',
+        position: '1'
+    },
+    {
+        id: '2',
+        name: 'Andrés Camilo Loaiza Clement',
+        result: '74,52%',
+        position: '2'
+    },
+    {
+        id: '3',
+        name: 'Andrea Paola Jaramillo Zapata',
+        result: '70,05%',
+        position: '3'
+    },
+]
+
 export default function SeeResult() {
 
     return (
-        <section className="my-8 min-h-lvh">
+        <section className="flex flex-col lg:flex-row lg:justify-center gap-5 my-8 mx-3 min-h-lvh">
             <section className="max-w-screen-sm">
                 <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2">
                     <div className="flex text-20">
@@ -56,10 +77,8 @@ export default function SeeResult() {
                         }
                     </div>
                 </AccordionTest>
-            </section>
 
-            <section className="max-w-screen-sm mt-8">
-                <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2">
+                <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2 mt-8">
                     <p className="font-semibold text-20 text-principal-150 py-1">Temas a Mejorar</p>
                 </div>
                 <p className="bg-principal-200 rounded py-2 px-2">
@@ -118,6 +137,35 @@ export default function SeeResult() {
                         </div>
                     </AccordionTest>
                 </div>
+            </section>
+
+            <section className="max-w-screen-md w-full">
+                <div className="flex justify-between gap-6 bg-principal-100 rounded-4 py-5 px-2">
+                    <p className="font-semibold text-20 text-principal-150 py-1">Resultado por Estudiante</p>
+                </div>
+                <div className="bg-principal-150 pt-2 pb-5">
+                    <div className="flex justify-between border-b border-b-principal-50 py-2 px-2  w-full">
+                        <p className="font-semibold w-85">Estudiante</p>
+                        <p className="font-semibold px-1">Puntaje</p>
+                        <p className="font-semibold px-1">Puesto</p>
+                        <p className="font-semibold px-1">Informe</p>
+                    </div>
+                    {
+                        exampleStudents.map((student, index) => (
+                            <div key={index} className="flex justify-between border-b border-b-principal-50 py-1.5 w-full px-2 even:bg-principal-200">
+                                <p className="w-[345px]">{student.name}</p>
+                                <p className="px-1">{student.result}</p>
+                                <p className="px-1">{student.position}</p>
+                                <p
+                                    className="text-principal-100 border-b w-fit pX-1"
+                                >
+                                    Ver Más
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>
+
             </section>
         </section>
     )
